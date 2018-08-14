7ea6f33e045eb5142a491093b0abef4c8f8b44c2
Eduardo Cortez <educortez91@gmail.com>
2018-08-13 18:28:32 -0600

    Add project files.

---
+++ DataMusicOrganizer/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace DataMusicOrganizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Url = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.File = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Title = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Artist = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Album = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Genre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TrackNumber = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lblSongsFound = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.lblSongPlaying = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Url,
+            this.File,
+            this.Title,
+            this.Artist,
+            this.Album,
+            this.Genre,
+            this.TrackNumber});
+            this.dataGridView1.Location = new System.Drawing.Point(26, 89);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(780, 300);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEndEdit);
+            this.dataGridView1.CellMouseDown += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_CellMouseDown);
+            this.dataGridView1.CellMouseEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellMouseEnter);
+            this.dataGridView1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.dataGridView1_KeyPress);
+            // 
+            // Url
+            // 
+            this.Url.HeaderText = "Url";
+            this.Url.Name = "Url";
+            this.Url.Visible = false;
+            // 
+            // File
+            // 
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.DimGray;
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.Gainsboro;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.SteelBlue;
+            this.File.DefaultCellStyle = dataGridViewCellStyle2;
+            this.File.HeaderText = "File";
+            this.File.Name = "File";
+            this.File.ReadOnly = true;
+            this.File.Width = 150;
+            // 
+            // Title
+            // 
+            this.Title.HeaderText = "Title";
+            this.Title.Name = "Title";
+            this.Title.Width = 150;
+            // 
+            // Artist
+            // 
+            this.Artist.HeaderText = "Artist";
+            this.Artist.Name = "Artist";
+            this.Artist.Width = 150;
+            // 
+            // Album
+            // 
+            this.Album.HeaderText = "Album";
+            this.Album.Name = "Album";
+            this.Album.Width = 150;
+            // 
+            // Genre
+            // 
+            this.Genre.HeaderText = "Genre";
+            this.Genre.Name = "Genre";
+            // 
+            // TrackNumber
+            // 
+            this.TrackNumber.HeaderText = "Track N°";
+            this.TrackNumber.Name = "TrackNumber";
+            this.TrackNumber.Width = 75;
+            // 
+            // lblSongsFound
+            // 
+            this.lblSongsFound.AutoSize = true;
+            this.lblSongsFound.ForeColor = System.Drawing.Color.White;
+            this.lblSongsFound.Location = new System.Drawing.Point(23, 391);
+            this.lblSongsFound.Name = "lblSongsFound";
+            this.lblSongsFound.Size = new System.Drawing.Size(35, 13);
+            this.lblSongsFound.TabIndex = 1;
+            this.lblSongsFound.Text = "label1";
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.BackColor = System.Drawing.Color.DimGray;
+            this.btnLoad.FlatAppearance.BorderColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btnLoad.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLoad.ForeColor = System.Drawing.Color.LightGray;
+            this.btnLoad.Location = new System.Drawing.Point(26, 60);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(104, 23);
+            this.btnLoad.TabIndex = 2;
+            this.btnLoad.Text = "Load Music Folder";
+            this.btnLoad.UseVisualStyleBackColor = false;
+            this.btnLoad.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(570, 39);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(236, 44);
+            this.axWindowsMediaPlayer1.TabIndex = 7;            
+            this.axWindowsMediaPlayer1.PlayStateChange += new AxWMPLib._WMPOCXEvents_PlayStateChangeEventHandler(this.axWindowsMediaPlayer1_PlayStateChange);            
+            // 
+            // lblSongPlaying
+            // 
+            this.lblSongPlaying.AutoSize = true;
+            this.lblSongPlaying.ForeColor = System.Drawing.Color.White;
+            this.lblSongPlaying.Location = new System.Drawing.Point(159, 70);
+            this.lblSongPlaying.Name = "lblSongPlaying";
+            this.lblSongPlaying.Size = new System.Drawing.Size(35, 13);
+            this.lblSongPlaying.TabIndex = 8;
+            this.lblSongPlaying.Text = "label2";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DimGray;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.SystemColors.AppWorkspace;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.LightGray;
+            this.button2.Location = new System.Drawing.Point(662, 395);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(144, 21);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Set Text in Selected Cells";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Gainsboro;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Location = new System.Drawing.Point(506, 396);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(150, 20);
+            this.textBox1.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(43)))), ((int)(((byte)(43)))));
+            this.ClientSize = new System.Drawing.Size(835, 468);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lblSongPlaying);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.lblSongsFound);
+            this.Controls.Add(this.dataGridView1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "[Murganizer]";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lblSongsFound;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button btnLoad;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Label lblSongPlaying;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Url;
+        private System.Windows.Forms.DataGridViewTextBoxColumn File;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Title;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Artist;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Album;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Genre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TrackNumber;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ DataMusicOrganizer/Form1.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace DataMusicOrganizer
+{
+    public partial class Form1 : Form
+    {
+        int i;
+        int row_playing;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {            
+            dataGridView1.AllowUserToAddRows = false;
+            lblSongsFound.Text = "";
+            lblSongPlaying.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string selectedPath = folderBrowserDialog1.SelectedPath;
+                Cursor.Current = Cursors.WaitCursor;
+                LoadMusic(selectedPath);
+                Cursor.Current = Cursors.Default;
+            }
+        }
+
+        public void LoadMusic(string dir)
+        {
+            dataGridView1.Rows.Clear();
+            TagLib.File f2 = null;
+            FileInfo f = null;
+            FileSystemInfo f1 = null;
+            bool path_B;
+            string path;
+            String[] s1 = Directory.GetFiles(dir, "*.mp3", SearchOption.AllDirectories);
+            int j = 0;
+            for (i = 0; i < s1.Length; i++)
+            {
+                f = new FileInfo(s1[i]);
+                f1 = new FileInfo(s1[i]);
+                path_B = f1.Exists;
+                if (path_B && f.Length != 0)
+                {                    
+                    try
+                    {
+                        path = f1.FullName;
+                        f2 = TagLib.File.Create(path);
+                        if (!f2.PossiblyCorrupt)
+                        {
+                            dataGridView1.Rows.Add();
+                            dataGridView1.Rows[j].Cells[0].Value = path;
+                            dataGridView1.Rows[j].Cells[1].Value = f1.Name;
+                            dataGridView1.Rows[j].Cells[2].Value = f2.Tag.Title;
+                            dataGridView1.Rows[j].Cells[3].Value = f2.Tag.FirstPerformer;
+                            dataGridView1.Rows[j].Cells[4].Value = f2.Tag.Album;
+                            dataGridView1.Rows[j].Cells[5].Value = f2.Tag.JoinedGenres;
+                            dataGridView1.Rows[j].Cells[6].Value = f2.Tag.Track.ToString();
+                            j++;
+                        }
+                    }
+                    catch (TagLib.CorruptFileException ex)
+                    {
+                        MessageBox.Show((f1.FullName + " " + ex.Message), "Hey!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }            
+            f2.Dispose();
+            if(j==1)
+                lblSongsFound.Text = "You have found " + j.ToString() + " file.";
+            else
+                lblSongsFound.Text = "You have found " + j.ToString() + " files.";
+        }
+
+
+
+        private void dataGridView1_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                string path = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+                TagLib.File f = TagLib.File.Create(path);
+                switch (e.ColumnIndex)
+                {
+                    case 2:
+                        if (dataGridView1.CurrentRow.Cells[2].Value == null)
+                            f.Tag.Title = "";
+                        else
+                            f.Tag.Title = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+                        break;
+                    case 3:
+                        if (dataGridView1.CurrentRow.Cells[3].Value == null)
+                            f.Tag.Performers = new[] { "" };
+                        else
+                            f.Tag.Performers = new[] { dataGridView1.CurrentRow.Cells[3].Value.ToString() };
+                        break;
+                    case 4:
+                        if (dataGridView1.CurrentRow.Cells[4].Value == null)
+                            f.Tag.Album = "";
+                        else
+                            f.Tag.Album = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+                        break;
+                    case 5:
+                        if (dataGridView1.CurrentRow.Cells[5].Value == null)
+                            f.Tag.Genres = new[] { "" };
+                        else
+                            f.Tag.Genres = new[] { dataGridView1.CurrentRow.Cells[5].Value.ToString() };
+                        break;
+                    case 6:
+                        if (dataGridView1.CurrentRow.Cells[6].Value == null)
+                            f.Tag.Track = 0;
+                        else
+                            f.Tag.Track = Convert.ToUInt32(dataGridView1.CurrentRow.Cells[6].Value.ToString());
+                        break;
+                }
+                f.Save();
+            }
+            catch (IOException ex)
+            {
+                SetCorrectValuesAfterError();
+                MessageBox.Show(ex.Message, "Oops :(", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (UnauthorizedAccessException un)
+            {
+                SetCorrectValuesAfterError();
+                MessageBox.Show(un.Message, "Oops :(", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void SetCorrectValuesAfterError()
+        {
+            string path = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            TagLib.File f = TagLib.File.Create(path);
+            if (!f.PossiblyCorrupt)
+            {
+                string title = f.Tag.Title;
+                string artist = f.Tag.FirstPerformer;
+                string album = f.Tag.Album;
+                string genre = f.Tag.JoinedGenres;
+                string trackn = f.Tag.Track.ToString();
+                dataGridView1.CurrentRow.Cells[2].Value = title;
+                dataGridView1.CurrentRow.Cells[3].Value = artist;
+                dataGridView1.CurrentRow.Cells[4].Value = album;
+                dataGridView1.CurrentRow.Cells[5].Value = genre;
+                dataGridView1.CurrentRow.Cells[6].Value = trackn;
+            }
+        }
+
+        private void SetCorrectValuesAfterMultipleErrors(int r)
+        {
+            string path = dataGridView1.Rows[r].Cells[0].Value.ToString();
+            TagLib.File f = TagLib.File.Create(path);
+            if (!f.PossiblyCorrupt)
+            {
+                string title = f.Tag.Title;
+                string artist = f.Tag.FirstPerformer;
+                string album = f.Tag.Album;
+                string genre = f.Tag.JoinedGenres;
+                string trackn = f.Tag.Track.ToString();
+                dataGridView1.Rows[r].Cells[2].Value = title;
+                dataGridView1.Rows[r].Cells[3].Value = artist;
+                dataGridView1.Rows[r].Cells[4].Value = album;
+                dataGridView1.Rows[r].Cells[5].Value = genre;
+                dataGridView1.Rows[r].Cells[6].Value = trackn;
+            }
+        }
+
+        private void dataGridView1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (dataGridView1.Rows.Count > 0)
+            {
+                if (e.KeyChar == Convert.ToChar(Keys.Enter))
+                {
+                    DataGridViewCell cell = dataGridView1.Rows[dataGridView1.CurrentRow.Index - 1].Cells[dataGridView1.CurrentCell.ColumnIndex];
+                    dataGridView1.CurrentCell = cell;
+                    dataGridView1.BeginEdit(true);
+                }
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == 1 && e.RowIndex >= 0)
+            {
+                axWindowsMediaPlayer1.URL = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+                if (dataGridView1.CurrentRow.Cells[2].Value != null && dataGridView1.CurrentRow.Cells[3].Value != null)
+                    lblSongPlaying.Text = "Now playing: " + dataGridView1.CurrentRow.Cells[3].Value.ToString() + " - " + dataGridView1.CurrentRow.Cells[2].Value.ToString();
+                else
+                {
+                    lblSongPlaying.Text = "Now playing: " + dataGridView1.CurrentRow.Cells[1].Value.ToString();
+                }
+            }
+        }
+
+        private void dataGridView1_CellMouseEnter(object sender, DataGridViewCellEventArgs e)
+        {
+
+            var dataGridView = (sender as DataGridView);
+
+            if (e.ColumnIndex >= 0 && e.ColumnIndex == 1&&e.RowIndex>=0)
+                dataGridView.Cursor = Cursors.Hand;
+            else
+                dataGridView.Cursor = Cursors.Default;
+            
+        }
+
+        private void axWindowsMediaPlayer1_PlayStateChange(object sender, AxWMPLib._WMPOCXEvents_PlayStateChangeEvent e)
+        {
+            if (e.newState == 3)
+            {
+                dataGridView1.Rows[row_playing].ReadOnly = false;
+                row_playing = dataGridView1.CurrentRow.Index;
+                dataGridView1.CurrentRow.ReadOnly = true;                
+            }
+
+            if (e.newState == 8 || e.newState == 1)
+            {                
+                axWindowsMediaPlayer1.URL = "";
+                lblSongPlaying.Text = "";
+                dataGridView1.Rows[row_playing].ReadOnly = false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length < 1)
+                MessageBox.Show("Please write... Something.", "Hey,", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            foreach (DataGridViewCell r in dataGridView1.SelectedCells)
+            {
+                r.Value = textBox1.Text;
+                try
+                {
+                    string path = dataGridView1.Rows[r.RowIndex].Cells[0].Value.ToString();
+                    TagLib.File f = TagLib.File.Create(path);
+                    switch (r.ColumnIndex)
+                    {
+                        case 2:
+                            if (dataGridView1.Rows[r.RowIndex].Cells[2].Value == null)
+                                f.Tag.Title = "";
+                            else
+                                f.Tag.Title = dataGridView1.Rows[r.RowIndex].Cells[2].Value.ToString();
+                            break;
+                        case 3:
+                            if (dataGridView1.Rows[r.RowIndex].Cells[3].Value == null)
+                                f.Tag.Performers = new[] { "" };
+                            else
+                                f.Tag.Performers = new[] { dataGridView1.Rows[r.RowIndex].Cells[3].Value.ToString() };
+                            break;
+                        case 4:
+                            if (dataGridView1.Rows[r.RowIndex].Cells[4].Value == null)
+                                f.Tag.Album = "";
+                            else
+                                f.Tag.Album = dataGridView1.Rows[r.RowIndex].Cells[4].Value.ToString();
+                            break;
+                        case 5:
+                            if (dataGridView1.Rows[r.RowIndex].Cells[5].Value == null)
+                                f.Tag.Genres = new[] { "" };
+                            else
+                                f.Tag.Genres = new[] { dataGridView1.Rows[r.RowIndex].Cells[5].Value.ToString() };
+                            break;
+                        case 6:
+                            if (dataGridView1.Rows[r.RowIndex].Cells[6].Value == null)
+                                f.Tag.Track = 0;
+                            else
+                                f.Tag.Track = Convert.ToUInt32(dataGridView1.Rows[r.RowIndex].Cells[6].Value.ToString());
+                            break;
+                    }
+                    f.Save();
+                }
+                catch (IOException ex)
+                {
+                    SetCorrectValuesAfterMultipleErrors(r.RowIndex);
+                    MessageBox.Show(ex.Message, "Oops :(", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                catch (UnauthorizedAccessException un)
+                {
+                    SetCorrectValuesAfterMultipleErrors(r.RowIndex);
+                    MessageBox.Show(un.Message, "Oops :(", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "";
+        }
+
+        private void dataGridView1_CellMouseDown(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                if (dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value != null)
+                {
+                    textBox1.Text = dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString();
+                }
+            }
+        }
+    }
+}
